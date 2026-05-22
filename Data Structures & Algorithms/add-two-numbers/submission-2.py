dbc4b15ac2550261cc4bc9215dbf9a97da055ee2
Peixin Shi <123029707+Doglily3@users.noreply.github.com>
2026-05-21 21:55:07 -0400

    Add: add-two-numbers - submission-2

---
+++ Data Structures & Algorithms/add-two-numbers/submission-2.py
@@ -0,0 +1,44 @@
+class Solution:
+    def addTwoNumbers(self, l1, l2):
+
+        node1 = l1
+        node2 = l2
+
+        n1 = 0
+        n2 = 0
+
+        index1 = 1
+        index2 = 1
+
+        # linked list -> number
+        while node1:
+            n1 += node1.val * index1
+            index1 *= 10
+            node1 = node1.next
+
+        while node2:
+            n2 += node2.val * index2
+            index2 *= 10
+            node2 = node2.next
+
+        result = n1 + n2
+
+        # number -> linked list
+        dummy = ListNode(0)
+        cur = dummy
+
+        # 特殊情况：0
+        if result == 0:
+            return ListNode(0)
+
+        while result > 0:
+
+            digit = result % 10
+
+            cur.next = ListNode(digit)
+
+            cur = cur.next
+
+            result //= 10
+
+        return dummy.next
